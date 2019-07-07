0c445011fcad3516bf2675e322524ef3ec1e41a4
Edward Korkollie <korkollieed@gmail.com>
2019-07-07 14:19:03 -0400

    update

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,7 +8,29 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+
+    public Integer selection(Integer startIndex, String selection) {
+        if(playList[startIndex].equals(selection)) {
+            return 0;
+        }
+
+        Integer up = startIndex;
+        Integer down = startIndex;
+
+        for(int step = 1; step <= playList.length/2; step++) {
+            up++;
+            down--;
+
+            if(down == -1) {
+                down = playList.length-1;
+            }
+            if(up == playList.length) {
+                up =0;
+            }
+            if(playList[down].equals(selection) || playList[up].equals(selection)) {
+                return step;
+            }
+        }
+        return -1;
     }
 }
